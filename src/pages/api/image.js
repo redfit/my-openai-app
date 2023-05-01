735a369318de6bc2bf64f00b875aa12eb3fd3139
daisuke fuchiwaki <fuchiwaki.d@note.jp>
2023-05-01 18:17:51 +0900

    Generate Images with AI in Node.js Using the OpenAI SDK & DALL-E

--- src/pages/api/image.js
+++ src/pages/api/image.js
@@ -7,6 +7,11 @@ const configuration = new Configuration({
 const openai = new OpenAIApi(configuration);
 
 export default async function handler(req, res) {
-  const response = await openai.listEngines();
+  const response = await openai.createImage({
+    prompt: "噴火する富士山",
+    n: 1,
+    size: "512x512",
+  });
+
   res.status(200).json({ data: response.data.data });
 }
